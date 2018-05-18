4a52118f82e8dd574a9e8f7ba6c6671cdc4ef5bd
John McDermott <jem@atmc.net>
2018-05-18 17:05:24 -0400

    Added visibleCardButtons

--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -17,7 +17,7 @@ class ConcentrationViewController: VCLLoggingViewController
     private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
 
     var numberOfPairsOfCards: Int {
-        return (cardButtons.count + 1) / 2
+        return (visibleCardButtons.count + 1) / 2
     }
 
     private(set) var flipCount = 0 {
@@ -43,9 +43,13 @@ class ConcentrationViewController: VCLLoggingViewController
 
     @IBOutlet private var cardButtons: [UIButton]!
 
+    private var visibleCardButtons: [UIButton]! {
+        return cardButtons.filter { !$0.superview!.isHidden }
+    }
+
     @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
-        if let cardNumber = cardButtons.index(of: sender) {
+        if let cardNumber = visibleCardButtons.index(of: sender) {
             game.chooseCard(at: cardNumber)
             updateViewFromModel()
         }else {
@@ -54,9 +58,9 @@ class ConcentrationViewController: VCLLoggingViewController
     }
 
     private func updateViewFromModel() {
-        if cardButtons != nil{
-            for index in cardButtons.indices {
-                let button = cardButtons[index]
+        if visibleCardButtons != nil{
+            for index in visibleCardButtons.indices {
+                let button = visibleCardButtons[index]
                 let card = game.cards[index]
                 if card.isFaceUp {
                     button.setTitle(emoji(for: card), for: UIControlState.normal)
